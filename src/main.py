e95e985dfd24fb2edad6627314011eed03dcfe6f
George Mihov <gmihov001@gmail.com>
2021-03-24 20:18:19 +0000

    Updates

--- src/main.py
+++ src/main.py
@@ -67,6 +67,13 @@ def login():
     access_token = create_access_token(identity=email)
     return jsonify(access_token), 200
 
+@app.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200    
+
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
